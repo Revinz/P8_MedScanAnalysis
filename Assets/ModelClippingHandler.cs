08cb4050e87f36e9171910fbb564bc2f5e57a035
Revinz <revinz1405@gmail.com>
2021-03-06 08:46:54 +0100

    Clipping Shader

---
+++ Assets/ModelClippingHandler.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ModelClippingHandler : MonoBehaviour
+{
+
+    //Properties used for the shader interaction
+    public Vector3 ClipMax = new Vector3(1, 1, 1);
+    public Vector3 ClipMin = new Vector3(0, 0, 0);
+
+    private Vector3 boundsMax;
+    private Vector3 boundsMin;
+
+    private List<ModelMesh> ModelMeshes;
+
+    void OnEnable() {
+        Debug.Log("Clipping handler setup - ");
+        SetupShaders();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateObjectBoundsInfo();
+        updateShaderProperties();
+    }
+
+    // Gets all the renderes and sets up the material property block
+    // which allows for seperate instance values for the shader properties
+    void SetupShaders() {
+        ModelMeshes = new List<ModelMesh>();
+
+        foreach (Transform child in transform) {
+           ModelMesh mesh = child.GetComponent<ModelMesh>();
+           mesh.SetupMesh();
+           ModelMeshes.Add(mesh);
+        }  
+
+        //Allows for the model being 1 big model instead of multiple meshes
+        ModelMesh rootModelMesh = GetComponent<ModelMesh>();
+        if (rootModelMesh != null) {
+            rootModelMesh.SetupMesh();
+            ModelMeshes.Add(rootModelMesh);
+        }
+    }
+
+    // Finds the max and min bounderies for the model.
+    void updateObjectBoundsInfo() {
+        Vector3 tmpMax = ModelMeshes[0].rend.bounds.max;
+        Vector3 tmpMin = ModelMeshes[0].rend.bounds.min;;
+        foreach (ModelMesh mesh in ModelMeshes)
+        {   
+            //Compare and get max bounds
+            tmpMax.x = Mathf.Max(tmpMax.x, mesh.rend.bounds.max.x);
+            tmpMax.y = Mathf.Max(tmpMax.y, mesh.rend.bounds.max.y);
+            tmpMax.z = Mathf.Max(tmpMax.z, mesh.rend.bounds.max.z);
+
+            //Compare and get min bounds
+            tmpMin.x = Mathf.Min(tmpMin.x, mesh.rend.bounds.min.x);
+            tmpMin.y = Mathf.Min(tmpMin.y, mesh.rend.bounds.min.y);
+            tmpMin.z = Mathf.Min(tmpMin.z, mesh.rend.bounds.min.z);
+        }
+
+        boundsMax = tmpMax;
+        boundsMin = tmpMin;
+    }
+
+    //Updates all the clipping shaders in the gameobject tree with the model
+    void updateShaderProperties() {
+
+        foreach (ModelMesh mesh in ModelMeshes)
+        {
+            mesh.UpdateShaderProperties(boundsMax, boundsMin, ClipMax, ClipMin);
+        }
+
+    }
+}
+
---
+++ Assets/ModelMesh.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelMesh : MonoBehaviour
+{   
+    public Color meshColor = Color.white;
+    [HideInInspector]
+    public Renderer rend;
+    private MaterialPropertyBlock propBlock;
+
+    public void SetupMesh() {
+        Debug.Log("Mesh setup");
+        rend = GetComponent<MeshRenderer>();
+        propBlock = new MaterialPropertyBlock();
+    }
+
+    public void UpdateShaderProperties(Vector3 boundsMax, Vector3 boundsMin,
+                                         Vector3 ClipMax, Vector3 ClipMin)
+    {
+        //Debug.Log(i);
+        rend.GetPropertyBlock(propBlock);
+
+        //Update boundary box and clipping positions in the shader
+        propBlock.SetVector("BoundsMax", boundsMax);
+        propBlock.SetVector("BoundsMin", boundsMin);
+        propBlock.SetVector("ClipMax", ClipMax);
+        propBlock.SetVector("ClipMin", ClipMin);
+
+        //Other props
+        propBlock.SetVector("ClippingModelColor", meshColor);
+
+        rend.SetPropertyBlock(propBlock); 
+    }
+}
+
